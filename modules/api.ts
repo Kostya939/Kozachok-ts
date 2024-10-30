aa90d4f35d95d1ec1a00ce4106637d2435d00296
Kostya939 <91373707+Kostya939@users.noreply.github.com>
2024-10-30 18:48:48 +0200

    Update api.ts

--- modules/api.ts
+++ modules/api.ts
@@ -6,7 +6,6 @@ export async function fetchData(): Promise<void> {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         const data: Post[] = await response.json();
         console.log(data);
-        // Тут можна додати код для відображення даних на сторінці
     } catch (error) {
         console.error("Error fetching data", error);
     }
